63a8f46df22b4d12382798b78b238d22054f920c
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2021-04-18 18:38:34 +0800

    Create cards.py

---
+++ cards.py
@@ -0,0 +1,7 @@
+class Card:
+  # Here you can set your own cards
+  def __init__(name, description, atk, hp):
+    self.name = name
+    self.description = description
+    slef.atk = atk
+    self.hp = hp
