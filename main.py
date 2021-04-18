0eaa83529d70fd9e9ce06a66d88d04b098823bde
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2021-04-18 18:52:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ from random import choice
 # Card Dictionary (edit here!)
 cards = [card("AlphaBeta", "AlphaBeta is a GOD, yeah right a GOD, BOW DOWN TO HIM.", 1000, 1000),
          card("Underdog", "Underdog is a monster/GOD hybrid.", 180, 70)]
+
+c.showCardData(cards[0])
+c.showCardData(cards[1])
