99787cadcc015085ec7352d5bb54c4d38964642d
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2021-04-19 18:58:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # The code you can modify.
 
 # Import all the classes (dont change)
-import card as c
+from cards import *
 from random import choice
 
 # Card Dictionary (edit here!)
-cards = [card("AlphaBeta", "AlphaBeta is a GOD, yeah right a GOD, BOW DOWN TO HIM.", 1000, 1000),
-         card("Underdog", "Underdog is a monster/GOD hybrid.", 180, 70)]
+cards = [Card("AlphaBeta", "AlphaBeta is a GOD, yeah right a GOD, BOW DOWN TO HIM.", 1000, 1000),
+  Card("Underdog", "Underdog is a monster/GOD hybrid.", 180, 70)]
 
-c.showCardData(cards[0])
-c.showCardData(cards[1])
+cards[0].showCardData()
+cards[1].showCardData()
