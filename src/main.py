55fc5e5fbf3daffbb4dab14e2dae3234f9ea2b2a
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2021-04-22 19:39:17 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,11 +2,14 @@
 
 # Import all the classes (dont change)
 from cards import *
-from random import choice
+from random import choices
 
 # Card Dictionary (edit here!)
 cards = [Card("AlphaBeta", "AlphaBeta is a GOD, yeah right a GOD, BOW DOWN TO HIM.", 1000, 1000),
-  Card("Underdog", "Underdog is a monster/GOD hybrid.", 180, 70)]
+  Card("Underdog", "Underdog is a monster/GOD hybrid.", 180, 70),
+  Item("EPIC GAMER!!!1111", "ok", 100, 100)]
 
-cards[0].showCardData()
-cards[1].showCardData()
+deck = choices(cards, k=5)
+print("Player deck:")
+for x in deck:
+  x.showCardData()
