e96a5181b2bb050ff1d2d3adc391979fc922d8bb
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2021-05-09 15:06:55 +0800

    Update player.py

--- src/player.py
+++ src/player.py
@@ -1,7 +1,7 @@
 class Player:
-  def __init__(points:int, cards:list):
-    self.points = points
+  def __init__(self, cards=[]):
+    self.points = 0
     self.cards = cards
     
-  def addPoint():
+  def addPoint(self):
     self.points += 1
