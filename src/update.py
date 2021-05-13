c56e4438370a82c4b1a76ba0a09f41ce8c2f61ee
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2021-05-13 18:24:15 +0800

    Update update.py

--- src/update.py
+++ src/update.py
@@ -8,7 +8,7 @@ class Update:
     x = requests.get("https://raw.githubusercontent.com/AlphaBeta906/Modifiable-TCG/main/src/update.py")
 
     if x[3] != open("update.py", "r"):
-      print(f"Do you want to update the souce code, Version {}? (y/n)/nNOTE: DATABASES WONT BE CHANGED".format(x[3].replace('__version__' = ', ''))
+      print(f"Do you want to update the souce code, Version {}? (y/n)/nNOTE: DATABASES WONT BE CHANGED".format(x[3].replace('__version__ = ', ''))
       desision = input("> ")
 
       if desision == "y":
