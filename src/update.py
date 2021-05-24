d89182b4c2e55bd004f9893fcf609f669bbc9b3b
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2021-05-24 09:07:21 +0800

    Update update.py

--- src/update.py
+++ src/update.py
@@ -1,24 +1,14 @@
-import sys
-import os
+import requests
 
 __version__ = "V1.1.2"
 programs = ["main.py", "player.py", "card.py", "update.py"]
 
 class Update:
   
-  def __init__(self):
-    try:
-      import requests
-    except ModuleNotFoundError:
-      print('Please down requests for the starting of the update code, please do so by using "python -m pip install requests" or alternatively "pip install requests"')
-      sys.exit()
-      
+  def __init__(self): 
     x = requests.get("https://raw.githubusercontent.com/AlphaBeta906/Modifiable-TCG/main/src/update.py")
-
-    with open('update.py', 'r') as f:
-        data = f.read()
         
-    if x.text.splitlines()[2] != f:
+    if x.text.splitlines()[2] != __version__:
       newVersion = x.text.splitlines()[2].replace('__version__ = ', '').strip('\"')
       print(f"Do you want to update the souce code, to Version {newVersion}? (y/n)\nNOTE: DATABASES WONT BE CHANGED")
       desision = input("> ")
@@ -28,8 +18,7 @@ class Update:
         for code in programs:
           x = requests.get("https://raw.githubusercontent.com/AlphaBeta906/Modifiable-TCG/main/src/main.py")
           open("main.py", "w").write(x.text)
-          file_size = os.getsize(x.txt)
-          print (f"Sucessfully imported: {code} {newVersion} ({file_size})")
+          print (f"Sucessfully imported: {code} {newVersion}")
 
         print ("Version {newVersion} is now imported\nPLEASE CHECK GITHUB REPO FOR NEW FILES TO DOWNLOAD")
       else:
